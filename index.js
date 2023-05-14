7153a89143d5bbaed78363b86b4d811c8434b5ce
Jeremy Ferrer <jeremy.eferrer@gmail.com>
2023-05-14 15:53:00 +1000

    Overhaul of caching approach

--- index.js
+++ index.js
@@ -1,72 +1,76 @@
 const express = require('express');
-const redis = require('redis');
+const NodeCache = require('node-cache');
 const responseTime = require('response-time');
 const axios = require('axios');
+const cron = require('node-cron')
 require('dotenv').config();
 
 const app = express();
 
+const cache = new NodeCache({ stdTTL: 60 * 5 }); // TTL of 5 minutes
 const port = process.env.PORT || 3000;
-const redisHost = process.env.REDIS_HOST;
-const redisPort = process.env.REDIS_PORT;
-const redisUsername = process.env.REDIS_USERNAME;
-const redisPassword = process.env.REDIS_PASSWORD;
 
 const ihsHost = process.env.IHS_HOST;
 const ihsNamespace = process.env.IHS_NAMESPACE;
 const ihsUsername = process.env.IHS_USERNAME;
 const ihsPassword = process.env.IHS_PASSWORD;
 
-const client = redis.createClient({
-  legacyMode: true,
-  socket: {
-    host: redisHost,
-    port: redisPort,
-  },
-  username: redisUsername,
-  password: redisPassword,
-});
+const validCodes = process.env.VALID_CODES.split(',')
 
-client.connect()
+// Generate and cache and API key on startup
+// There will always be an API key in cache from startup
+startup()
 
-const isCached = (req, res, next) => {
-  const fundname = req.params.fundname;
-  client.get(fundname, (err, data) => {
-    if (err) throw err;
+function startup(){
+  ihs_authenticateUser(ihsHost, ihsUsername, ihsPassword)
+  .then((results)=>{
+    cache.set(`ihs_apikey`, results, 3500);
+    console.log('STARTUP: API Key Cached')
+    console.log('STARTUP: Creating scheduled tasks')
 
-    if (data !== null) {
-      console.log(`Serving Cache`)
-      data = JSON.parse(data);
-      res.json({ status: 'success', fundname, data });
-    } else {
-        
-      next();
-    }
-  });
-};
-
-const isAuthenticated = (req, res, next) => {
-  client.get('ihs_apikey', (err, data) => {
-    if (err) throw err;
-
-    if (data !== null) {
-      console.log(`API Key:Serving cache`)
-      res.locals.apikey = data
-      next();
-    } else {
-      console.log(`API Key: Reauthenticating and saving to cache`)
-      ihs_authenticateUser(ihsHost, ihsUsername, ihsPassword)
-      .then((results)=>{
-        client.setEx(`ihs_apikey`, 3500, results);
-        res.locals.apikey = results
-        next();
-      })
-      .catch(err=>{
-        res.status(400).json(err)
+    for(let i = 0; i < validCodes.length;i++){
+      console.log(`\tSCHEDULER: Adding scheduled job - Update Fund Data for ${validCodes[i]}`)
+      cron.schedule('*/3 * * * * *', ()=>{
+        try{
+          const fundcode = validCodes[i]
+          const cachedKey = cache.get('ihs_apikey')
+    
+          getFundData(fundcode,cachedKey)
+            .then(results=>{
+              cache.set(fundcode, results, 5);
+              console.log(`SCHEDULER: Funds data for ${fundcode} refreshed`)
+            })
+            .catch(err=>{
+              throw err
+            })
+        }
+        catch (err){
+          console.log(err)
+        }
       })
     }
-  });
-};
+    console.log(`\tSCHEDULER: Adding scheduled job - Update API Key`)
+    cron.schedule('*/55 * * * *', ()=>{
+      try{
+        ihs_authenticateUser(ihsHost, ihsUsername, ihsPassword)
+          .then(results=>{
+            cache.set(`ihs_apikey`, results, 3500);
+            console.log('SCHEDULER: API Key Cached')
+          })
+          .catch(err=>{
+            throw err
+          })
+      }
+      catch (err){
+        console.log(err)
+      }
+    })
+  })
+  .catch(err=>{
+    console.log(err)
+    process.exit()
+  })
+}
 
 async function ihs_authenticateUser(host, username, password) {
   const url = `${host}/apikey`;
@@ -77,7 +81,7 @@ async function ihs_authenticateUser(host, username, password) {
   } catch (error) {
     throw {message: `IHS API Error: ${error.response.data.errorMessage}`}
   }
-}
+};
 
 async function ihs_getlatestfund(host, namespace, apikey, fundTicker) {
   const url = `${host}/${namespace}/Fund/latest`;
@@ -89,21 +93,64 @@ async function ihs_getlatestfund(host, namespace, apikey, fundTicker) {
       return response.data[0];
     }
     else{
-
       return {}
     }
   } catch (error) {
     console.error(error)
     throw {message: `IHS API Error: ${error.response.data.errorMessage}`}
   }
-}
+};
+
+const isAuthenticated = (req, res, next) => {
+  const cachedKey = cache.get('ihs_apikey')
+
+  if(cachedKey) {
+    console.log('AUTH: API in cache')
+    res.locals.apikey = cachedKey
+    next()
+  }
+  else {
+    console.log(`AUTH: API NOT in cache - Reauthenticating and saving to cache`)
+    ihs_authenticateUser(ihsHost, ihsUsername, ihsPassword)
+    .then((results)=>{
+      cache.set(`ihs_apikey`, results, 3500);
+      res.locals.apikey = results
+      next();
+    })
+    .catch(err=>{
+      res.status(400).json(err)
+    })
+  }
+};
+
+const isAllowed = (req, res, next) => {
+  const fundname = req.params.fundname;
+
+  if(!validCodes.includes(fundname)){
+    res.status(404)
+    res.json({ status: 'not found' });
+  }
+  else{
+    next()
+  }
+};
+
+const isCached = (req, res, next) => {
+  const fundname = req.params.fundname;
+  const cachedData = cache.get(fundname);
+
+  if(cachedData != undefined){
+    console.log(`WEB: Serving Cache`)
+    res.json({ status: 'success', fundname, data: cachedData });
+  }
+  else{
+    next()
+  }
+};
 
 const getFundData = async (fundname, apikey) => {
-  console.log('Getting fund data')
   const data = await ihs_getlatestfund(ihsHost, ihsNamespace, apikey, fundname)
   if (data !== null) {
-    console.log(`Setting Cache`)
-    client.setEx(fundname, 2, JSON.stringify(data));
     return data;
   } else {
     return {};
@@ -113,15 +160,24 @@ const getFundData = async (fundname, apikey) => {
 app.use(responseTime());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(function (req, res, next) {
+  res.header("Content-Type",'application/json');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.json({ status: 'Server running' });
 });
 
-app.get('/fund/:fundname', isCached, isAuthenticated, async (req, res) => {
+app.get('/fund/:fundname', isAllowed, isCached, isAuthenticated, async (req, res) => {
+
   const fundname = req.params.fundname;
   try {
     const data = await getFundData(fundname, res.locals.apikey);
+    console.log(`CACHE: Setting Cache`)
+    cache.set(fundname, data, 3);
+
+
     res.json({ status: 'success', fundname, data });
   } catch (err) {
     res.json({ status: 'error', message: err.message });
@@ -140,5 +196,4 @@ app.use((err, req, res, next) => {
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
-  console.log(`Redis Client Open: ${client.isOpen}`);
 });
